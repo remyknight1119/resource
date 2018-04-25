853d64a099b12b667addd95c399a33df0912d697
remy <ytliu@fortinet.com>
2018-04-25 11:19:58 +0800

    get_ip
    Signed-off-by: remy <ytliu@fortinet.com>

--- code/get_ip.c
+++ code/get_ip.c
@@ -1,132 +1,77 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <net/if.h>
-#include <sys/ioctl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <errno.h>
-
-int getLocalInfo(void)
+#include <arpa/inet.h>
+#include <string.h>
+ 
+unsigned char get_netmask_prefix(unsigned char *mask, int len)
 {
-    int fd;
-    int interfaceNum = 0;
-    struct ifreq buf[16];
-    struct ifconf ifc;
-    struct ifreq ifrcopy;
-    char mac[16] = {0};
-    char ip[32] = {0};
-    char broadAddr[32] = {0};
-    char subnetMask[32] = {0};
+    int                 i;
+    int                 j;
+    unsigned char       prefix = 0;
 
-    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
-        perror("socket");
-
-        close(fd);
-        return -1;
-    }
-
-    ifc.ifc_len = sizeof(buf);
-    ifc.ifc_buf = (caddr_t)buf;
-    if (!ioctl(fd, SIOCGIFCONF, (char *)&ifc))
-    {
-        interfaceNum = ifc.ifc_len / sizeof(struct ifreq);
-        printf("interface num = %d\n", interfaceNum);
-        while (interfaceNum-- > 0)
-        {
-            printf("ndevice name: %s\n", buf[interfaceNum].ifr_name);
-
-            //ignore the interface that not up or not runing  
-            ifrcopy = buf[interfaceNum];
-            if (ioctl(fd, SIOCGIFFLAGS, &ifrcopy))
-            {
-                printf("ioctl: %s [%s:%d]\n", strerror(errno), __FILE__, __LINE__);
-
-                close(fd);
-                return -1;
+    for (i = 0 ; i < len; i++) {
+        for (j = 7;  j >= 0; j--) {
+            if ((mask[i] & (1 << j)) == 0) {
+                return prefix;
             }
-
-            //get the mac of this interface  
-            if (!ioctl(fd, SIOCGIFHWADDR, (char *)(&buf[interfaceNum])))
-            {
-                memset(mac, 0, sizeof(mac));
-                snprintf(mac, sizeof(mac), "%02x%02x%02x%02x%02x%02x",
-                    (unsigned char)buf[interfaceNum].ifr_hwaddr.sa_data[0],
-                    (unsigned char)buf[interfaceNum].ifr_hwaddr.sa_data[1],
-                    (unsigned char)buf[interfaceNum].ifr_hwaddr.sa_data[2],
-
-                    (unsigned char)buf[interfaceNum].ifr_hwaddr.sa_data[3],
-                    (unsigned char)buf[interfaceNum].ifr_hwaddr.sa_data[4],
-                    (unsigned char)buf[interfaceNum].ifr_hwaddr.sa_data[5]);
-                printf("device mac: %s\n", mac);
-            }
-            else
-            {
-                printf("ioctl: %s [%s:%d]\n", strerror(errno), __FILE__, __LINE__);
-                close(fd);
-                return -1;
-            }
-
-            //get the IP of this interface  
-
-            if (!ioctl(fd, SIOCGIFADDR, (char *)&buf[interfaceNum]))
-            {
-                snprintf(ip, sizeof(ip), "%s",
-                    (char *)inet_ntoa(((struct sockaddr_in *)&(buf[interfaceNum].ifr_addr))->sin_addr));
-                printf("device ip: %s\n", ip);
-            }
-            else
-            {
-                printf("ioctl: %s [%s:%d]\n", strerror(errno), __FILE__, __LINE__);
-                close(fd);
-                return -1;
-            }
-
-            //get the broad address of this interface  
-
-            if (!ioctl(fd, SIOCGIFBRDADDR, &buf[interfaceNum]))
-            {
-                snprintf(broadAddr, sizeof(broadAddr), "%s",
-                    (char *)inet_ntoa(((struct sockaddr_in *)&(buf[interfaceNum].ifr_broadaddr))->sin_addr));
-                printf("device broadAddr: %s\n", broadAddr);
-            }
-            else
-            {
-                printf("ioctl: %s [%s:%d]\n", strerror(errno), __FILE__, __LINE__);
-                close(fd);
-                return -1;
-            }
-            //get the subnet mask of this interface  
-            if (!ioctl(fd, SIOCGIFNETMASK, &buf[interfaceNum]))
-            {
-                snprintf(subnetMask, sizeof(subnetMask), "%s",
-                    (char *)inet_ntoa(((struct sockaddr_in *)&(buf[interfaceNum].ifr_netmask))->sin_addr));
-                printf("device subnetMask: %s\n", subnetMask);
-            }
-            else
-            {
-                printf("ioctl: %s [%s:%d]\n", strerror(errno), __FILE__, __LINE__);
-                close(fd);
-                return -1;
-
-            }
+            prefix++;
         }
     }
-    else
-    {
-        printf("ioctl: %s [%s:%d]\n", strerror(errno), __FILE__, __LINE__);
-        close(fd);
-        return -1;
-    }
-  
-    close(fd);
+    return prefix;
+}
 
+int get_if_addr_info(char *ifname)
+{
+    struct ifaddrs* ifaddr = NULL;
+    struct sockaddr_in *in;
+    struct sockaddr_in *m;
+    struct sockaddr_in6 *in6;
+    struct sockaddr_in6 *m6;
+    char ip[256];
+    unsigned char mask;
+    int ret = getifaddrs(&ifaddr);
+    struct ifaddrs* ifp = ifaddr;
+
+    if (ret) {
+        printf("getifaddrs failed, errno:%d\n", errno);
+        return 1;
+    }
+
+    for (; ifp != NULL; ifp = ifp->ifa_next) {
+        if (ifp->ifa_addr == NULL || ifp->ifa_netmask == NULL) {
+            continue;
+        }
+
+        if (strcmp(ifname, ifp->ifa_name) != 0) {
+            continue;
+        }
+
+        if (ifp->ifa_addr->sa_family == AF_INET) {
+            in = (struct sockaddr_in*)ifp->ifa_addr;
+            m = (struct sockaddr_in*)ifp->ifa_netmask;
+            inet_ntop(AF_INET, &in->sin_addr, ip, sizeof(ip));
+            mask = get_netmask_prefix((unsigned char *)&m->sin_addr, sizeof(m->sin_addr));
+            printf("dev:%s, ip:%s/%u\n", ifp->ifa_name, ip, mask);
+        } else {
+            in6 = (struct sockaddr_in6*)ifp->ifa_addr;
+            m6 = (struct sockaddr_in6*)ifp->ifa_netmask;
+            inet_ntop(AF_INET6, &in6->sin6_addr, ip, sizeof(ip));
+            mask = get_netmask_prefix((unsigned char *)&m6->sin6_addr, sizeof(m6->sin6_addr));
+            printf("dev:%s, ip:%s/%u\n", ifp->ifa_name, ip, mask);
+        }
+    }
+
+    freeifaddrs(ifaddr);
     return 0;
 }
-  
+
 int main(void)
 {
-    getLocalInfo();
-
-    return 0;
+    return get_if_addr_info("enp5s0");
 }
