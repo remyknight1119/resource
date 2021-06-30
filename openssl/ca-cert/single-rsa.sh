795f97c9454878c6582b45b03144460c677aa3fc
remy <remyknight1119@gmail.com>
2021-06-30 14:28:35 -0700

    add ca-cert sample
    Signed-off-by: remy <remyknight1119@gmail.com>

---
+++ openssl/ca-cert/single-rsa.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+dir=`dirname $0`
+#cd $dir
+
+set -e
+key_bits=4096
+expire_days=3650
+subj=/C="CN"/ST="Beijing"/L="RSA1"/O="Fortinet"/OU="ecdsa"/CN="domain3.net"
+ca_name=ca-root
+root_cacer=$ca_name.cer
+root_cakey=$ca_name.key
+param=rsa-single2
+cacer=$ca_name.cer
+cakey=$ca_name.key
+cer=$param.cer
+csr=$param.csr
+key=$param.key
+p12=$param.p12
+pfx=$param.pfx
+config=./openssl.cnf
+
+#Server cert
+openssl genrsa -out $key $key_bits
+openssl req -new -key $key -sha256 -out $csr -subj $subj -days $expire_days
+openssl x509 -req -in $csr -sha256 -out $cer -CA $cacer -CAkey $cakey -CAserial t_ssl_ca.srl -CAcreateserial -days $expire_days -extensions v3_req
+#openssl ca -config $config -keyfile $key -out $cer -infiles $csr
+#openssl pkcs12 -export -clcerts -in $cer -inkey $key -out $p12
+#openssl pkcs12 -export -in $cer -inkey $key -out $pfx 
+
+#cat $sub1_cacer $cacer $cer $key |tee $param.pem
+cat $cer $key | tee $param.pem
+echo "===================Gen All OK===================="
+# openssl verify -CAfile ca.cer client.cer
+
