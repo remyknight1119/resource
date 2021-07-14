51fc5a0cfa623d89ea84d47e397316e085d3e1d4
remy <remyknight1119@gmail.com>
2021-07-14 10:25:40 -0700

    update vmmon.sh
    Signed-off-by: remy <remyknight1119@gmail.com>

--- scripts/vmmon.sh
+++ scripts/vmmon.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -e
 priv=vmmon.priv
 der=vmmon.der
 openssl req -new -x509 -newkey rsa:2048 -keyout $priv -outform DER -out $der -nodes -days 36500 -subj "/CN=VMware/"
-sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 $priv $der $(modinfo -n vmmon)
-sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 $priv $der $(modinfo -n vmnet)
+headers=/usr/src/linux-headers-
+headers=/usr/src/kernels/
+sudo $headers`uname -r`/scripts/sign-file sha256 $priv $der $(modinfo -n vmmon)
+sudo $headers`uname -r`/scripts/sign-file sha256 $priv $der $(modinfo -n vmnet)
 sudo mokutil --import $der
